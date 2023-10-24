1eff6fbe50fd957fc8963fd97c2b887102259ebd
Goran SD <g.cvetic23@gmail.com>
2023-10-24 07:18:46 -0700

    axios token injection added

--- utils/api/api.jsx
+++ utils/api/api.jsx
@@ -4,4 +4,18 @@ const api = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
+// Add a request interceptor to include the JWT in the headers
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+
+  // Add a response interceptor to refresh the JWT if it expired
+);
+
 export default api;

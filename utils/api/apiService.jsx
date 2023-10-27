76d4881ac0e5a3027ccb671190b9686d17e8c4c6
Goran SD <g.cvetic23@gmail.com>
2023-10-26 20:11:02 -0700

    api calls extracted into shared component

--- utils/api/apiService.jsx
+++ utils/api/apiService.jsx
@@ -1,8 +1,46 @@
-import api from "./axiosClient";
+import axiosClient from "./axiosClient";
 
+// Fetch products
 export const fetchProducts = async () => {
   try {
-    const response = await api.get("/products");
+    const response = await axiosClient.get("/products");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// User Login
+export const userLogin = async (username, password) => {
+  try {
+    const response = await axiosClient.post("/user/login", {
+      username,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// User Signup
+export const userSignUp = async (
+  firstName,
+  lastName,
+  cellNumber,
+  email,
+  password
+) => {
+  try {
+    const response = await axiosClient.post("/user/signup", {
+      firstName,
+      lastName,
+      cellNumber,
+      email,
+      password,
+    });
     return response.data;
   } catch (error) {
     console.error(error);

d65b5c6dffebfdf5b5d2da858b046291b1865a4b
Goran SD <g.cvetic23@gmail.com>
2023-10-26 20:11:56 -0700

    page routing updated

---
+++ app/user/login/page.jsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { userLogin } from "@/utils/api/apiService";
+import { handleApiError } from "@/utils/api/errorHandeling";
+// import supabaseClient from "../../utils/authentication/supabaseClient";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await userLogin(username, password);
+      localStorage.setItem("token", data.token);
+      router.push("/inventory");
+    } catch (error) {
+      handleApiError(error, setErrorMsg);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-1/3 p-8 bg-white rounded-lg shadow-md">
+        <h1 className="mb-4 text-2xl text-center">Sign In</h1>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Type your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              placeholder="Type your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="w-full p-2 text-white bg-green-500 rounded"
+            >
+              Submit
+            </button>
+          </div>
+          {errorMsg && (
+            <div className="text-center text-red-500">{errorMsg}</div>
+          )}
+        </form>
+        <div className="flex flex-col mt-2 text-center">
+          <Link href="/forgot-password" passHref>
+            Forgot your password?
+          </Link>
+          <Link href="/signup" passHref>
+            Don't have an account? Sign up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ app/user/signup/page.jsx
@@ -0,0 +1,90 @@
+"use client";
+import React, { useState } from "react";
+import { userSignUp } from "@/utils/api/apiService";
+
+const SignUpForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [cellNumber, setCellNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      await userSignUp(firstName, lastName, cellNumber, email, password);
+      router.push("user/login");
+    } catch (error) {
+      handleApiError(error, setErrorMsg);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-1/3 p-8 bg-white rounded-lg shadow-md">
+        <h1 className="mb-4 text-2xl text-center">Sign Up</h1>
+        <form onSubmit={handleSignUp}>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="tel"
+              placeholder="Cell Number"
+              value={cellNumber}
+              onChange={(e) => setCellNumber(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="w-full p-2 text-white bg-green-500 rounded"
+            >
+              Submit
+            </button>
+          </div>
+          {errorMsg && (
+            <div className="text-center text-red-500">{errorMsg}</div>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;

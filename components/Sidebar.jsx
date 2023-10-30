bfe56c69e2978dbb769e1518f01bfcf7d94b81c7
Goran SD <g.cvetic23@gmail.com>
2023-10-30 00:16:47 -0700

    sidebar navigation underline is dynamic now

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,13 +1,27 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { logoutModal as Modal } from "./Modals/logoutModal";
 import { userLogout } from "@/utils/api/apiService";
+import classNames from "classnames";
 
 const Sidebar = () => {
   const [showModal, setShowModal] = useState(false);
   const router = useRouter();
+  const pathname = usePathname();
+
+  // Function to check if the current route is the same as the path
+  const isCurrentRoute = (path) => pathname === path;
+
+  // Sidebar navigation items
+  const navItems = [
+    { label: "Dashboard", path: "/dashboard", icon: "/images/home.png" },
+    { label: "Inventory", path: "/inventory", icon: "/images/inventory.png" },
+    { label: "Suppliers", path: "/suppliers", icon: "/images/supplier.png" },
+    { label: "Orders", path: "/orders", icon: "/images/orders.png" },
+    { label: "Reports", path: "/reports", icon: "/images/reports.png" },
+  ];
 
   const handleLogout = async (shouldLogout) => {
     setShowModal(false);
@@ -42,67 +56,38 @@ const Sidebar = () => {
           </div>
         </div>
         <div className="items-center flex-grow pt-10 pl-12 space-y-10 overflow-y-auto text-xl h-80">
-          <div
-            className="flex flex-row space-x-3 cursor-pointer hover:text-blue-500"
-            onClick={() => router.push("/dashboard")}
-          >
-            <Image
-              src="/images/home.png"
-              alt="Home picture"
-              width={30}
-              height={30}
-            />
-            <div>Dashboard</div>
-          </div>
-          <div
-            className="flex flex-row space-x-3 text-blue-600 underline cursor-pointer hover:text-blue-500"
-            onClick={() => router.push("/inventory")}
-          >
-            <Image
-              src="/images/inventory.png"
-              alt="Inventory picture"
-              width={30}
-              height={30}
-            />
-            <div>Inventory</div>
-          </div>
-          <div
-            className="flex flex-row space-x-3 cursor-pointer hover:text-blue-500 "
-            onClick={() => router.push("/suppliers")}
-          >
-            <Image
-              src="/images/supplier.png"
-              alt="Sales picture"
-              width={30}
-              height={30}
-            />
-            <div>Suppliers</div>
-          </div>
-          <div
-            className="flex flex-row space-x-3 cursor-pointer hover:text-blue-500"
-            onClick={() => router.push("/orders")}
-          >
-            <Image
-              src="/images/orders.png"
-              alt="Orders picture"
-              width={30}
-              height={30}
-            />
-            <div>Orders</div>
-          </div>
-          <div
-            className="flex flex-row space-x-3 cursor-pointer hover:text-blue-500"
-            onClick={() => router.push("/reports")}
-          >
-            <Image
-              src="/images/reports.png"
-              alt="Reports picture"
-              width={30}
-              height={30}
-            />
-            <div>Reports</div>
-          </div>
+          {navItems.map((item, index) => {
+            console.log(
+              "Current route: ",
+              router.pathname,
+              "Check route: ",
+              item.path,
+              "Is match:",
+              isCurrentRoute(item.path)
+            );
+            return (
+              <div
+                key={index}
+                className={classNames(
+                  "flex flex-row space-x-3 cursor-pointer hover:text-blue-500",
+                  {
+                    "text-blue-600 underline": isCurrentRoute(item.path),
+                  }
+                )}
+                onClick={() => router.push(item.path)}
+              >
+                <Image
+                  src={item.icon}
+                  alt={`${item.label} picture`}
+                  width={30}
+                  height={30}
+                />
+                <div>{item.label}</div>
+              </div>
+            );
+          })}
         </div>
+
         <div className="flex flex-row pb-8 pl-12 text-xl cursor-pointer hover:text-blue-500">
           <Image
             src="/images/logout.png"

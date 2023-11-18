1fac041eca14ceaf7c080b286ba5f9d271d85a9b
Goran SD <g.cvetic23@gmail.com>
2023-11-18 11:00:57 -0800

    Refactor authentication logic in Home component

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,26 @@
 "use client";
+
+import React, { useEffect } from "react";
 import MainLayout from "@/layouts/MainLayout";
-import { useRedirectToInventory } from "@/utils/hooks/useRedirectToInventory";
-import InventoryPage from "./inventory/page";
-import DashboardPage from "./dashboard/page";
 import LandingPageLayout from "@/layouts/LandingPageLayout";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  //this is temporary redirect to inventory page
-  // useRedirectToInventory("/inventory"); // Redirects to /inventory
-
-  // const [hasToken, setHasToken] = useState(false);
-
-  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      router.push("/inventory");
-    }
-    console.log("User Auth token", token);
+    const checkAuthStatus = async () => {
+      try {
+        const response = await validateUser();
+        if (response === "Authenticated") {
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        console.error("Authentication check failed", error);
+        setIsAuthenticated(false);
+      }
+    };
+    checkAuthStatus();
   }, []);
 
-  return <LandingPageLayout />;
+  return isAuthenticated ? <MainLayout /> : <LandingPageLayout />;
 }

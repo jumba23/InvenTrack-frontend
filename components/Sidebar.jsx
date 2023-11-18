74f74d3b195e0f6e47288cca76e9b9c5a6c72cd5
Goran SD <g.cvetic23@gmail.com>
2023-11-18 11:01:26 -0800

    Remove localStorage.removeItem("token") in Sidebar component

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -29,7 +29,6 @@ const Sidebar = () => {
       try {
         await userLogout();
 
-        localStorage.removeItem("token");
         router.push("/");
       } catch (error) {
         console.error("Failed to logout:", error);

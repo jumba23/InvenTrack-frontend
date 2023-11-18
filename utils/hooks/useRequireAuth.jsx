123a7558812065fadcdbbec1dbfea1c5a08d6b5a
Goran SD <g.cvetic23@gmail.com>
2023-11-18 12:21:21 -0800

    Fix authentication redirect issue

--- utils/hooks/useRequireAuth.jsx
+++ utils/hooks/useRequireAuth.jsx
@@ -4,17 +4,20 @@ import { validateUser } from "../api/apiService";
 
 export const useRequireAuth = (redirectUrl) => {
   const router = useRouter();
+  console.log("useRequireAuth", redirectUrl);
 
   useEffect(() => {
     const checkAuthStatus = async () => {
       try {
         const response = await validateUser();
         if (response === "Authenticated") {
+          console.log("response", response);
           router.push(redirectUrl);
         }
       } catch (error) {
         console.error("Authentication check failed", error);
         // Handle not authenticated scenario, like redirect to login
+        router.push("/");
       }
     };
     checkAuthStatus();

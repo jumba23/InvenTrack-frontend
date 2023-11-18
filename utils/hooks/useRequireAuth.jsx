0bc7281f6d1db37b266066b1593f9fa8fb2a0939
Goran SD <g.cvetic23@gmail.com>
2023-11-18 11:00:50 -0800

    Refactor useRequireAuth hook to include authentication check

--- utils/hooks/useRequireAuth.jsx
+++ utils/hooks/useRequireAuth.jsx
@@ -1,13 +1,22 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { validateUser } from "../api/apiService";
 
-export const useRequireAuth = (redirectUrl = "/") => {
+export const useRequireAuth = (redirectUrl) => {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      router.push(redirectUrl);
-    }
-  }, []);
+    const checkAuthStatus = async () => {
+      try {
+        const response = await validateUser();
+        if (response === "Authenticated") {
+          router.push(redirectUrl);
+        }
+      } catch (error) {
+        console.error("Authentication check failed", error);
+        // Handle not authenticated scenario, like redirect to login
+      }
+    };
+    checkAuthStatus();
+  }, [router, redirectUrl]);
 };

a69514fc23af2bce8323e5815fa353954d9879d1
Goran SD <g.cvetic23@gmail.com>
2023-11-19 12:02:02 -0800

    Update logoutModal component styling

--- components/Modals/logoutModal.jsx
+++ components/Modals/logoutModal.jsx
@@ -5,15 +5,15 @@ export const logoutModal = ({ show, onClose }) => {
     <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
       <div className="p-8 bg-white rounded-lg">
         <h1 className="mb-4 text-xl">Are you sure you want to logout?</h1>
-        <div className="flex justify-between">
+        <div className="flex justify-evenly">
           <button
-            className="p-2 text-white bg-red-500 rounded"
+            className="w-20 p-2 text-white bg-red-500 rounded"
             onClick={() => onClose(true)}
           >
             Yes
           </button>
           <button
-            className="p-2 text-black bg-gray-300 rounded"
+            className="w-20 p-2 text-black bg-gray-300 rounded"
             onClick={() => onClose(false)}
           >
             No

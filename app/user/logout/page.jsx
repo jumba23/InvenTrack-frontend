9d0d39d0b3496a6319eac34c327eec2aff81bbed
Goran SD <g.cvetic23@gmail.com>
2023-11-22 07:07:07 -0800

    Add logout functionality

---
+++ app/user/logout/page.jsx
@@ -0,0 +1,18 @@
+// import { useRouter } from "next/navigation";
+// import { userLogout } from "../../../utils/api/apiService";
+
+// const logout = async (shouldLogout) => {
+//   const router = useRouter();
+//   setShowModal(false);
+//   if (shouldLogout) {
+//     try {
+//       await userLogout();
+
+//       router.push("/");
+//     } catch (error) {
+//       console.error("Failed to logout:", error);
+//     }
+//   }
+// };
+
+// export default logout;

b07d17e0eaf995f9a89c6e17f8a6eda2c5b350ea
Goran SD <g.cvetic23@gmail.com>
2024-02-03 16:56:04 -0800

    Refactor AuthContext and add custom hook

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,8 +1,12 @@
 "use client";
+// use client indicates this module is intended for client-side usage only.
+
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { userLogout, validateUser } from "@/utils/api/apiService";
 import { useRouter } from "next/navigation";
 
+// Creation of AuthContext with default values. This context will manage the authentication state
+// throughout the application.
 const AuthContext = createContext({
   isAuthenticated: false,
   showLogoutModal: false,
@@ -12,50 +16,64 @@ const AuthContext = createContext({
   router: null,
 });
 
+/**
+ * useAuth is a custom hook that simplifies the access to AuthContext, allowing components
+ * to consume the authentication state and related functions easily.
+ *
+ * @returns {object} The current authentication context value.
+ */
 export function useAuth() {
   return useContext(AuthContext);
 }
 
+/**
+ * AuthProvider is a component that wraps the application's component tree to provide
+ * a global authentication context. It initializes and manages the authentication state,
+ * including user authentication status, logout functionality, and loading states.
+ *
+ * @param {object} { children } - The child components of the application that will have access to the auth context.
+ */
 export function AuthProvider({ children }) {
-  const router = useRouter();
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
-  const [loading, setLoading] = useState(true);
+  const router = useRouter(); // Use the Next.js router for redirects on logout.
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // State for tracking authentication status.
+  const [loading, setLoading] = useState(true); // State for tracking loading status during authentication checks.
   const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   useEffect(() => {
-    // Perform the authentication check when the component mounts.
+    // Effect to perform the authentication check when the component mounts.
     const checkAuthStatus = async () => {
       try {
-        const response = await validateUser();
-        setIsAuthenticated(response === "Authenticated");
+        const response = await validateUser(); // Call API to validate user session/token.
+        setIsAuthenticated(response === "Authenticated"); // Update authentication status based on API response.
       } catch (error) {
         console.error("Authentication check failed", error);
-        setIsAuthenticated(false);
+        setIsAuthenticated(false); // Set authentication status to false on error.
       } finally {
-        setLoading(false);
+        setLoading(false); // Set loading to false once the check is complete.
       }
     };
     checkAuthStatus();
   }, []);
 
-  // user "logout" action handler
+  // Handles user logout action.
   const logout = async () => {
     try {
-      await userLogout();
-      setIsAuthenticated(false);
-      setShowLogoutModal(false);
-      router.push("/");
+      await userLogout(); // Call API to perform logout.
+      setIsAuthenticated(false); // Reset authentication status.
+      setShowLogoutModal(false); // Hide logout modal.
+      router.push("/"); // Redirect to home page on logout.
     } catch (error) {
       console.error("Logout Failed:", error);
     }
   };
 
-  // toggle logout modal
+  // Toggles the visibility of the logout modal.
   const toggleLogoutModal = () => {
     setShowLogoutModal(!showLogoutModal);
   };
 
-  // value object to be passed to the AuthContext.Provider
+  // The value object to be passed to the AuthContext.Provider, making these states and functions
+  // available to any component in the application that consumes this context.
   const value = {
     isAuthenticated,
     showLogoutModal,
@@ -64,8 +82,6 @@ export function AuthProvider({ children }) {
     loading,
     router,
   };
-
-  console.log("AuthProvider: Value", value);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }

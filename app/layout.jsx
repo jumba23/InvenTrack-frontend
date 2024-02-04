14089ed6d3699530f03d2d8ebe2681bfc84bc9c3
Goran SD <g.cvetic23@gmail.com>
2024-02-03 16:49:43 -0800

    Add AuthProvider to RootLayout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "../context/AuthContext"; // Ensure this path is correct
 import "./globals.css";
 
 // Metadata for the application.
@@ -10,13 +11,16 @@ export const metadata = {
 // RootLayout serves as the foundational layout component for the InvenTrack application.
 // It defines the basic HTML structure and is responsible for rendering children components.
 // This layout is typically used at the top level of the application to wrap around
-// the entire content, providing a consistent structure across different pages.
+// the entire content, providing a consistent structure across different pages
+// and integrating the AuthProvider for global authentication context.
 //------------------------------------------------------
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </AuthProvider>
   );
 }

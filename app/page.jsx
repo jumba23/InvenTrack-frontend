d2ca1b932750dcb8382420a93809e17930c57e43
Goran SD <g.cvetic23@gmail.com>
2024-05-20 16:45:16 -0700

    chore: Update Home component to dynamically render layout based on authentication status

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,26 @@
 
 import React from "react";
 import IndexLayout from "../layouts/IndexLayout";
+import LandingPageLayout from "@/layouts/LandingPageLayout";
+import MainLayout from "@/layouts/MainLayout";
+import { useAuth } from "@/context/AuthContext";
 
 //------------------------------------------------------
 // Functionality:
-// - Serves as the primary entry point for the InvenTrack application.
-// - Wraps the rendered layout components within the AuthProvider context to provide
-//   authentication-related state and functionalities to child components.
+// - This page serves as the entry point for the application.
 //
 // Usage:
-// - This component is automatically rendered as the default page due to Next.js's
-//   file-based routing system, representing the root of the application.
+// - The Home component is the default page that users see when they visit the application.
+
+/** ======================================== SUMMARY ========================================
+ * Home is the main entry point for the application, determining the layout to render based on
+ * the user's authentication status. It uses the useAuth hook to access the global authentication
+ * context and decide whether to render the MainLayout or LandingPageLayout.
+ * ==========================================================================================*/
 //------------------------------------------------------
 
 export default function Home() {
-  return <IndexLayout />;
+  const { isAuthenticated } = useAuth();
+
+  return <>{isAuthenticated ? <MainLayout /> : <LandingPageLayout />}</>;
 }

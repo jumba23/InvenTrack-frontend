f93adb570e4becf4b599a07377ae3498dd5e1ca5
Goran SD <g.cvetic23@gmail.com>
2024-05-21 14:41:19 -0700

    feat: Add new page for adding new inventory items

---
+++ app/inventory/add-new-product/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductForm from "@/components/Forms/ProductForm";
+
+const AddItemPage = () => {
+  return (
+    <div>
+      <h1>Add New Item</h1>
+      <ProductForm />
+    </div>
+  );
+};
+
+export default AddItemPage;

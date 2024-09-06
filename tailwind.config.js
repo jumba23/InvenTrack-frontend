3474b166fcb08efd0a381800bc3664758a6bcbe3
Goran SD <g.cvetic23@gmail.com>
2024-09-05 21:55:32 -0700

    Refactor: Add pulse animation to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        pulse: {
+          "0%, 100%": { transform: "scale(1)", opacity: "1" },
+          "50%": { transform: "scale(1.1)", opacity: "0.7" },
+        },
+      },
+      animation: {
+        pulse: "pulse 1.5s ease-in-out infinite",
+      },
     },
   },
   plugins: [],

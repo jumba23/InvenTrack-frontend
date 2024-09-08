dcc7dd1c109b6bec3432cfc8b72c457c09cfbb1e
Goran SD <g.cvetic23@gmail.com>
2024-09-08 13:24:29 -0700

    Refactor InfoCards import in inventory page component

--- app/inventory/page.jsx
+++ app/inventory/page.jsx
@@ -3,7 +3,7 @@
 import React, { useMemo, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { useProduct } from "@/context/ProductContext";
-import InfoCards from "./InfoCards";
+import InfoCards from "./infoCards";
 import LogoSpinner from "@/components/Spinners/LogoSpinner";
 import { deleteProduct, fetchProducts } from "@/utils/api/apiService";
 import {

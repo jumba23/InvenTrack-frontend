f4f1ed6018566efedccaea4d7247e8ef299d7470
Goran SD <g.cvetic23@gmail.com>
2024-09-08 18:12:34 -0700

    Refactor webpack resolve alias in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname),
-      "@components": path.resolve(__dirname, "components"),
-    };
-
-    // You can also add some logging to check the path aliases during build
-    console.log("Webpack Aliases:", config.resolve.alias);
-
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
 };

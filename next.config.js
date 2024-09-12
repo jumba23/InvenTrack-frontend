7916f7663401121e50fda8b421383e4045728155
Goran SD <g.cvetic23@gmail.com>
2024-09-12 09:41:45 -0700

    Add remotePatterns configuration for Supabase image URLs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const path = require("path");
 
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https", // Ensure the protocol is correct
+        hostname: "ocpiojdbxsgjollypfis.supabase.co", // Supabase domain for your images
+        pathname: "/storage/v1/object/sign/**", // Ensure the path matches your image URLs
+      },
+    ],
+  },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;

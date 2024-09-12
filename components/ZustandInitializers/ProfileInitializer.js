b386e150f4d1e5b62684887df3830b9c8cfbde6f
Goran SD <g.cvetic23@gmail.com>
2024-09-11 21:40:42 -0700

    Refactor ProfileInitializer to trigger initial profile loading on mount

--- components/ZustandInitializers/ProfileInitializer.js
+++ components/ZustandInitializers/ProfileInitializer.js
@@ -3,12 +3,25 @@
 import { useEffect } from "react";
 import { useProfile } from "@/utils/hooks/useProfile";
 
+/**
+ * ProfileInitializer Component
+ *
+ * This component is responsible for initializing the user profile data when the application loads.
+ * It uses the useProfile hook to trigger the initial loading of the user profile.
+ *
+ * @component
+ * @param {Object} props
+ * @param {React.ReactNode} props.children - Child components to be rendered
+ * @returns {React.ReactNode} The child components
+ */
 export default function ProfileInitializer({ children }) {
   const { loadProfile } = useProfile();
 
   useEffect(() => {
+    // Trigger initial profile loading when the component mounts
     loadProfile();
   }, [loadProfile]);
 
+  // Render children without modifying them
   return <>{children}</>;
 }

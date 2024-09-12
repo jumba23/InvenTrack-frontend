861ba130a1f4fb4ff7ed430ad16426d16221462e
Goran SD <g.cvetic23@gmail.com>
2024-09-11 21:40:37 -0700

    Refactor ZustandInitializers/ProductInitializer.js to trigger initial product loading on mount

--- components/ZustandInitializers/ProductInitializer.js
+++ components/ZustandInitializers/ProductInitializer.js
@@ -3,12 +3,25 @@
 import { useEffect } from "react";
 import { useProduct } from "@/utils/hooks/useProduct";
 
+/**
+ * ProductInitializer Component
+ *
+ * This component is responsible for initializing the product data when the application loads.
+ * It uses the useProduct hook to trigger the initial loading of products.
+ *
+ * @component
+ * @param {Object} props
+ * @param {React.ReactNode} props.children - Child components to be rendered
+ * @returns {React.ReactNode} The child components
+ */
 export default function ProductInitializer({ children }) {
   const { loadProducts } = useProduct();
 
   useEffect(() => {
+    // Trigger initial product loading when the component mounts
     loadProducts();
   }, [loadProducts]);
 
+  // Render children without modifying them
   return <>{children}</>;
 }

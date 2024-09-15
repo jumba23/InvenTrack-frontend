8702f98d91e10b58115999e8e40b9cc01f34ddf5
Goran SD <g.cvetic23@gmail.com>
2024-09-15 06:59:00 -0700

    Refactor errorTypes.js module to add documentation for error types

--- utils/errorHandling/errorTypes.js
+++ utils/errorHandling/errorTypes.js
@@ -1,3 +1,16 @@
+// utils/errorHandling/errorTypes.js
+
+/**
+ * errorTypes.js
+ *
+ * This module defines an enum of error types used throughout the application.
+ * It provides a consistent way to categorize and handle different types of errors.
+ *
+ * Usage:
+ * Import the ErrorTypes enum and use it when setting or checking error types.
+ * Example: import { ErrorTypes } from './errorTypes';
+ */
+
 export const ErrorTypes = {
   API_ERROR: "API_ERROR",
   NETWORK_ERROR: "NETWORK_ERROR",

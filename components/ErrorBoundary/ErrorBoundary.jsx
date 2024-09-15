5c4711ce21f33e3b6be6bf7d9d21c3c6717cd1b0
Goran SD <g.cvetic23@gmail.com>
2024-09-15 06:20:37 -0700

    Add ErrorBoundary component to handle errors in the application layout

---
+++ components/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ErrorDisplay from "../ErrorDisplay/ErrorDisplay";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+    // You can log the error to an error reporting service here
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorDisplay error={this.state.error} />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;

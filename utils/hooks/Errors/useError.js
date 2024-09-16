6eb811cd728e9f58db4e25319bf84df87b5f968f
Goran SD <g.cvetic23@gmail.com>
2024-09-16 14:02:08 -0700

    Refactor error handling in useError hook

---
+++ utils/hooks/Errors/useError.js
@@ -0,0 +1,23 @@
+// hooks/useError.js
+
+import { useState, useCallback } from "react";
+import { handleApiError } from "@/utils/api/errorHandling";
+
+export const useError = (initialCustomMessages = {}) => {
+  const [error, setError] = useState(null);
+
+  const handleError = useCallback(
+    (err, customMessages = {}) => {
+      const errorObj = handleApiError(err, setError, {
+        ...initialCustomMessages,
+        ...customMessages,
+      });
+      return errorObj;
+    },
+    [initialCustomMessages]
+  );
+
+  const clearError = useCallback(() => setError(null), []);
+
+  return { error, handleError, clearError };
+};

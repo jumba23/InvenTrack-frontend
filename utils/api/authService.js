ce1d46a005b305c6e14fbc118221edb4f8f34b75
Goran SD <g.cvetic23@gmail.com>
2024-09-16 11:47:48 -0700

    Refactor import statements and error handling in apiService and authService

--- utils/api/authService.js
+++ utils/api/authService.js
@@ -12,9 +12,11 @@ export const userLogin = async (email, password, setErrorMsg) => {
     const response = await axiosClient.post("/user/login", { email, password });
     return response.data;
   } catch (error) {
-    return handleApiError(error, setErrorMsg, {
+    console.error("Login error API SERVICE:", error);
+    const errorObj = handleApiError(error, setErrorMsg, {
       authError: "Login failed. Please check your credentials.",
     });
+    throw errorObj; // Throw the error instead of returning it
   }
 };
 
@@ -28,9 +30,10 @@ export const userSignUp = async (userData, setErrorMsg) => {
     const response = await axiosClient.post("/user/signup", userData);
     return response.data;
   } catch (error) {
-    return handleApiError(error, setErrorMsg, {
+    const errorObj = handleApiError(error, setErrorMsg, {
       serverError: "Failed to register the user. Please try again later.",
     });
+    throw errorObj; // Throw the error instead of returning it
   }
 };
 
@@ -43,9 +46,10 @@ export const userLogout = async (setErrorMsg) => {
     const response = await axiosClient.post("/user/logout", { logout: true });
     return response.data;
   } catch (error) {
-    return handleApiError(error, setErrorMsg, {
+    const errorObj = handleApiError(error, setErrorMsg, {
       authError: "Logout failed. Please try again.",
     });
+    throw errorObj; // Throw the error instead of returning it
   }
 };
 
@@ -60,8 +64,9 @@ export const validateUser = async (setErrorMsg) => {
     });
     return response.data;
   } catch (error) {
-    return handleApiError(error, setErrorMsg, {
+    const errorObj = handleApiError(error, setErrorMsg, {
       authError: "Failed to validate the token.",
     });
+    throw errorObj; // Throw the error instead of returning it
   }
 };

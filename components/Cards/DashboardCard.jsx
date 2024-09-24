2a546b2e1b85e3db40c99c1805523a7d51e4ee25
Goran SD <g.cvetic23@gmail.com>
2024-09-24 14:24:13 -0700

    Refactor Card component to add dynamic height and custom class support

--- components/Cards/DashboardCard.jsx
+++ components/Cards/DashboardCard.jsx
@@ -11,13 +11,15 @@ import React from "react";
  * @param {string} props.title - The title of the card
  * @param {React.ReactNode} props.children - The content to be rendered inside the card
  */
-const Card = ({ title, children }) => {
+const Card = ({ title, children, className }) => {
   return (
-    <div className="overflow-hidden bg-white rounded-lg shadow-md">
+    <div
+      className={`bg-white rounded-lg shadow-md overflow-hidden h-full ${className}`}
+    >
       <div className="px-4 pt-5 sm:px-6">
         <h2 className="text-lg font-medium text-gray-900">{title}</h2>
       </div>
-      <div className="px-4 py-5 sm:p-6">{children}</div>
+      <div className="px-4 py-5 sm:p-6 h-[calc(100%-4rem)]">{children}</div>
     </div>
   );
 };

6a9454ac6cc9f06e81192b890f40694cce59f9cc
Goran SD <g.cvetic23@gmail.com>
2024-09-24 14:23:55 -0700

    Refactor InventoryLayout component to remove unnecessary CSS class and improve responsiveness

--- app/inventory/layout.jsx
+++ app/inventory/layout.jsx
@@ -4,9 +4,7 @@ import MainLayout from "@/layouts/MainLayout";
 export default function InventoryLayout({ children }) {
   return (
     <MainLayout>
-      <div className="w-full h-full overflow-auto main-container">
-        {children}
-      </div>
+      <div className="h-full overflow-hidden">{children}</div>
     </MainLayout>
   );
 }

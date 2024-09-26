460abf957f6231ed9e5dd9302adc01b38fd04936
Goran SD <g.cvetic23@gmail.com>
2024-09-26 14:42:47 -0700

    Refactor import path for SuppliersLayout component

---
+++ app/suppliers/layout.jsx
@@ -0,0 +1,7 @@
+// app/suppliers/layout.js
+
+import PageLayout from "@/layouts/PageLayout";
+
+export default function SuppliersLayout({ children }) {
+  return <PageLayout>{children}</PageLayout>;
+}

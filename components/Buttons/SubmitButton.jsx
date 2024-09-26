dc5d4134880909182217054c22264699d1f37918
Goran SD <g.cvetic23@gmail.com>
2024-09-26 13:28:21 -0700

    Refactor SubmitButton component to add primary styling and make it reusable

---
+++ components/Buttons/SubmitButton.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * SubmitButton Component
+ *
+ * A reusable button component with primary styling.
+ * This is typically used for submit or primary actions in forms.
+ *
+ * @component
+ * @param {Object} props
+ * @param {string} props.children - The text content of the button
+ * @param {string} props.className - Additional classes to apply to the button
+ * @param {Function} props.onClick - The click handler function
+ */
+
+const SubmitButton = ({ children, className, onClick, ...props }) => {
+  return (
+    <button
+      className={`px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-blue-700 rounded-full transition-colors duration-300 ease-in-out hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 w-full ${className}`}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+SubmitButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default SubmitButton;

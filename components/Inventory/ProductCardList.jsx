40e27d0a16830a25b73bd7e64d00e2a1a2a5a769
Goran SD <g.cvetic23@gmail.com>
2024-09-25 21:31:20 -0700

    Refactor InventoryPage component to use ProductCardList for displaying products

---
+++ components/Inventory/ProductCardList.jsx
@@ -0,0 +1,41 @@
+// components/Inventory/ProductCardList.jsx
+
+import React from "react";
+import ProductCard from "@/components/Cards/ProductCard";
+
+/**
+ * ProductCardList Component
+ *f
+ * This component renders a list of product cards for mobile view.
+ * It handles the expansion state of each card and provides edit and delete functionality.
+ *
+ * @param {Object[]} products - Array of product data to be displayed
+ * @param {Object} expandedCards - Object tracking the expansion state of each card
+ * @param {function} onToggleExpand - Function to handle toggling card expansion
+ * @param {function} onEdit - Function to handle editing a product
+ * @param {function} onDelete - Function to handle deleting a product
+ */
+const ProductCardList = ({
+  products,
+  expandedCards,
+  onToggleExpand,
+  onEdit,
+  onDelete,
+}) => {
+  return (
+    <div className="pb-20 mt-4">
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+          expanded={expandedCards[product.id]}
+          onToggleExpand={onToggleExpand}
+          onEdit={onEdit}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProductCardList;

027b86d58a263e9838133bcba7bc2445ebc9fb1f
Goran SD <g.cvetic23@gmail.com>
2024-09-29 21:56:44 -0700

    Refactor SupplierDeleteConfirmationDialog component to optimize rendering and improve code structure

--- components/Suppliers/modals/SupplierDeleteConfirmationDialog.jsx
+++ components/Suppliers/modals/SupplierDeleteConfirmationDialog.jsx
@@ -1,22 +1,45 @@
-//components/Suppliers/modals/SupplierDeleteConfirmationDialog.jsx
+// components/Dialogs/DeleteConfirmationDialog.jsx
 
 import React from "react";
-import useSupplierStore from "@/stores/supplierStore";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import CancelButton from "@/components/Buttons/CancelButton";
+import SubmitButton from "@/components/Buttons/SubmitButton";
 
-const SupplierDeleteConfirmationDialog = ({ supplierId, onClose }) => {
-  const { deleteSupplier } = useSupplierStore();
-
-  const handleDelete = async () => {
-    await deleteSupplier(supplierId);
-    onClose();
-  };
-
+/**
+ * DeleteConfirmationDialog Component
+ *
+ * This component renders a confirmation dialog for deleting a product.
+ * It provides options to confirm or cancel the deletion action.
+ *
+ * @param {boolean} open - Whether the dialog is open
+ * @param {function} onClose - Function to handle closing the dialog
+ * @param {function} onConfirm - Function to handle confirming the deletion
+ */
+const SupplierDeleteConfirmationDialog = ({ open, onClose, onConfirm }) => {
   return (
-    <div className="modal">
-      <p>Are you sure you want to delete this supplier?</p>
-      <button onClick={handleDelete}>Yes</button>
-      <button onClick={onClose}>No</button>
-    </div>
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Confirm Delete</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Are you sure you want to delete this supplier? This action cannot be
+          undone.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <CancelButton onClick={onClose} className="w-full sm:w-auto">
+          Cancel
+        </CancelButton>
+        <SubmitButton onClick={onConfirm} className="w-full sm:w-auto">
+          Delete
+        </SubmitButton>
+      </DialogActions>
+    </Dialog>
   );
 };
 
